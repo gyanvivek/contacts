ccf9a46f1b70f4e4af50a74d46939f8801e95090
gyanvivek <gyanvivek123@gmail.com>
2020-10-04 22:33:37 +0530

    added express server

---
+++ build/precache-manifest.6c2f6571ff00ae0305be82d9a2856908.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32c9973eaaf80e5072e29ecdd2178ce3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d8657c32f68c595b4df",
+    "url": "/static/css/main.2550eb36.chunk.css"
+  },
+  {
+    "revision": "bc089bf72c21732b3c2d",
+    "url": "/static/js/2.67cb3aaf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.67cb3aaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d8657c32f68c595b4df",
+    "url": "/static/js/main.9e818169.chunk.js"
+  },
+  {
+    "revision": "2fa9e31451af5fbb06a3",
+    "url": "/static/js/runtime-main.c51d566f.js"
+  },
+  {
+    "revision": "f62b2422f3fc82ac7f46c0a1483b3ece",
+    "url": "/static/media/background.f62b2422.svg"
+  },
+  {
+    "revision": "067f446bac023ff1efdc54d156eeb933",
+    "url": "/static/media/contactimage.067f446b.svg"
+  },
+  {
+    "revision": "0f23a15e31d4c6b5d3245558a1310408",
+    "url": "/static/media/delete.0f23a15e.svg"
+  },
+  {
+    "revision": "452e3b6620ded5ff97ba68d0c05e67e2",
+    "url": "/static/media/google.452e3b66.svg"
+  },
+  {
+    "revision": "6f1b64668daa0cde1f9ea977083f2c17",
+    "url": "/static/media/header_bar.6f1b6466.svg"
+  },
+  {
+    "revision": "77efc25e8f90690b031c89c237ee0918",
+    "url": "/static/media/logout.77efc25e.svg"
+  }
+]);
